67ea8c15568e51f429a1a5367996dd3e3ceddcfe
olin_tp_ar <universalexperimental@protonmail.com>
2023-07-11 14:40:41 +0200

    insertProduct method

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
-	products: Array
+  products: Product[] = [];
+
+  insertProduct(title: string, description: string, price: number) {
+    const newProduct = new Product(
+      new Date().toString(),
+      title,
+      description,
+      price,
+    );
+    this.products.push(newProduct);
+  }
 }
